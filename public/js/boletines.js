838fc8168a821614a4c433335525c765ccb72523
unknown <yurikogm.15@gmail.com>
2024-03-08 18:09:56 -0600

    se suben cambios en boletines

--- public/js/boletines.js
+++ public/js/boletines.js
@@ -2,6 +2,27 @@
 const btn_desplegable = document.querySelectorAll(".btn_desplegable");
 const year_desplegable = document.querySelectorAll(".year_desplegable");
 
+const pdf_buttons = document.querySelectorAll(".view-pdf");
+const pdf_view = document.querySelector("#pdf-view");
+const main_pdf = document.querySelector("#main-pdf");
+const loader_spinner = document.querySelector("#loader-spinner");
+
+pdf_buttons.forEach(function(button){
+    button.addEventListener("click",view_pdf);            
+});
+
+function view_pdf(){
+    let pdf_button_parent = event.target.parentNode;
+    let pdf = pdf_button_parent.lastElementChild;
+    let pdf_src = pdf.getAttribute("src");
+    main_pdf.setAttribute("src",pdf_src);
+    pdf_view.classList.add("is-hidden"),
+    loader_spinner.classList.remove("is-hidden");
+    setTimeout(function(){
+        loader_spinner.classList.add("is-hidden");
+        main_pdf.classList.remove("is-hidden");                
+    },1000);
+}
 
 const activa_folder = (e)=>{
     try {
@@ -9,13 +30,12 @@ const activa_folder = (e)=>{
         let target = e.target.parentElement.parentElement.parentElement.classList.contains("btn_desplegable") ? e.target.parentElement.parentElement.parentElement :e.target.parentElement;
         let year = target.getAttribute("year");
         let boletines = document.querySelector(`#boletines_${year}`);
-        //console.log(boletines.classList.length,"Bolet");
+        
         if (boletines.classList.length  === 2) {
             boletines.classList.remove("is-hidden");
         } else {
             boletines.classList.add("is-hidden");
         }
-        console.log(target.getAttribute("year"));
     } catch (error) {
         console.log(error);
     }
@@ -27,13 +47,12 @@ const activ_year = (e)=>{
         let target = e.target.parentElement.parentElement.parentElement.classList.contains("year_desplegable") ? e.target.parentElement.parentElement.parentElement :e.target.parentElement;
         let year = target.getAttribute("year");
         let boletines = document.querySelector(`#boletines_${year}`);
-        //console.log(boletines.classList.length,"Bolet");
+        
         if (boletines.classList.length  === 2) {
             boletines.classList.remove("is-hidden");
         } else {
             boletines.classList.add("is-hidden");
         }
-        console.log(target.getAttribute("year"));
     } catch (error) {
         console.log(error);
     }
